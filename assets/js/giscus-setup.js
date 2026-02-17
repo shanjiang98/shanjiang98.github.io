9187324fabcecadbc897a702441425b9228c1542
shanjiang98 <hilljiang2001@gmail.com>
2026-02-17 00:37:04 +0000

    Deploying to gh-pages from @ shanjiang98/shanjiang98.github.io@8f9a6c9cec3978ea34896c3c7b30943299c65b8f 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "shanjiang98/shanjiang.github.io",
+    "data-repo": "shanjiang98/shanjiang98.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
